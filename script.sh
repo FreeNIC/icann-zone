b974237f583aeca1f30bb7db0ff03ea5b7a3202c
jupiter.imperialnic.com <jupiter@imperialnic.com>
2018-06-25 16:18:15 +0000

    Auto update 2018-06-25 (16:18)

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 current_date_time="`date '+%Y-%m-%d (%H:%M)'`";
 
-rm outputs/*
+/usr/bin/git pull origin master
 
 /usr/bin/wget -O outputs/root.zone https://www.internic.net/domain/root.zone
 /bin/sed '/NSEC\|RRSIG\|DNSKEY\|SOA\|^\./d' < outputs/root.zone > outputs/root.include.zone
 
 /usr/bin/git add --no-all .
-/usr/bin/git commit -m "Auto update $current_date_time"
+/usr/bin/git commit --allow-empty -m "Auto update $current_date_time"
 /usr/bin/git push origin master
