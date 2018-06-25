f4bf5f2189ce2ff170f8659be46bb0889d090bf2
jupiter.imperialnic.com <jupiter@imperialnic.com>
2018-06-25 16:12:43 +0000

    Auto update 2018-06-25 (16:12)

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 
 current_date_time="`date '+%Y-%m-%d (%H:%M)'`";
 
+rm outputs/*
+
 /usr/bin/wget -O outputs/root.zone https://www.internic.net/domain/root.zone
 /bin/sed '/NSEC\|RRSIG\|DNSKEY\|SOA\|^\./d' < outputs/root.zone > outputs/root.include.zone
 
